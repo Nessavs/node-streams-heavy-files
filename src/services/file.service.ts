a7cf1b93b4773b445aa42f2d03a07309a2228200
VanessaDaSilva <vanessa.silva@amo.delivery>
2025-09-10 17:57:18 -0300

    feat: add reading, transformation and writin

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,20 +1,54 @@
-import { createReadStream } from "fs";
-import { readFile } from "fs/promises";
+import { createReadStream, createWriteStream } from "fs"
+import { readFile } from "fs/promises"
+import { BotFrameworkConfig } from "microsoft-cognitiveservices-speech-sdk"
+import { createInterface } from "readline"
+import { Transform } from "stream"
+import { pipeline } from "stream/promises"
 
-const filename = "largeFile.csv";
+const filename = "largeFile.csv"
 
 async function brokenapp() {
-    await readFile(filename, "utf8");
+  await readFile(filename, "utf8")
 }
 
 function readLargeFile() {
-const readStream = createReadStream(filename, {encoding: "utf8"});
+  const readStream = createReadStream(filename, { encoding: "utf8" })
 
-readStream.on("data", (chunk) => {
+  readStream.on("data", (chunk) => {
     console.log(chunk);
-})
+  })
 }
 
-//brokenapp();
+function transformCsvLine(line: string) {
+  const parts = line.split(",")
+  if (parts.length === 3) {
+    parts[0] = parts[0].trim().toUpperCase()
+    const alterationDate = new Date().toISOString()
+    return [...parts, alterationDate].join(",") + "\n"
+  }
+  return line + "\n"
+}
 
-readLargeFile();
+async function processCsvFile(inputFilePath: string, outputFilePath: string) { 
+try {
+  const readStream = createReadStream(inputFilePath, { encoding: "utf8" })
+  const writeStream = createWriteStream(outputFilePath, { encoding: "utf8" })
+  const lineReader = createInterface({
+    input: readStream
+  })
+
+  const transformStream = new Transform({
+    objectMode: true,
+    transform(chunk: string, encoding, callback) {
+      callback(null, transformCsvLine(chunk))
+      //console.log(transformCsvLine(chunk))
+    }
+  })
+
+  pipeline(lineReader, transformStream, writeStream)
+
+} catch (error) {
+  console.log("Erro ao processar o CSV", error)
+}}
+
+processCsvFile(filename, "output.csv")
